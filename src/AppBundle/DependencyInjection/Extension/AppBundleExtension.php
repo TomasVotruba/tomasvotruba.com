b095b12fb7bdcbc43b026a9ccfa9358adca5d896
Tomas Votruba <tomas.vot@gmail.com>
2015-09-08 01:20:20 +0200

    init

---
+++ src/AppBundle/DependencyInjection/Extension/AppBundleExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\DependencyInjection\Extension;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class AppBundleExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
