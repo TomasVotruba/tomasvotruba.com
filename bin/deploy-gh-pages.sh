7b530432345fafb9707ba6d0fa314fe6ffd2874c
Tomas Votruba <tomas.vot@gmail.com>
2016-01-07 00:13:31 +0100

    Regenerated output

---
+++ bin/deploy-gh-pages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Build settings
+REPOSITORY=${REPOSITORY:-"https://${GH_TOKEN}@github.com/TomasVotruba/tomasvotruba.cz.git"}
+BRANCH=${BRANCH:-"gh-pages"}
+
+# Git identity
+GIT_AUTHOR_NAME=${GIT_AUTHOR_NAME:-"Travis"}
+GIT_AUTHOR_EMAIL=${GIT_AUTHOR_EMAIL:-"travis@travis-ci.org"}
+
+# Generate API
+git clone "${REPOSITORY}" "${BUILD_DIR}" --branch "${BRANCH}" --depth 1
+./vendor/bin/sculpin generate --env=prod
+
+# Commit & push
+cd ./output_prod
+git add .
+git commit -m "Regenerated output"
+git push origin "${BRANCH}"
