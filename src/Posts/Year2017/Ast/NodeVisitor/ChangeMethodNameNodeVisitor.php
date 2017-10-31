707a03f93aba4506b56ee528662b744fdc12a4ec
TomasVotruba <tomas.vot@gmail.com>
2017-10-31 21:11:54 +0100

    make tests pass

---
+++ src/Posts/Year2017/Ast/NodeVisitor/ChangeMethodNameNodeVisitor.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace TomasVotruba\Website\Posts\Year2017\Ast\NodeVisitor;
+
+use PhpParser\Node;
+use PhpParser\Node\Name;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\NodeVisitorAbstract;
+
+final class ChangeMethodNameNodeVisitor extends NodeVisitorAbstract
+{
+    public function enterNode(Node $node): ?Node
+    {
+        if (! $node instanceof ClassMethod) {
+            return $node;
+        }
+
+        $node->name = new Name('changedName');
+
+        return $node;
+    }
+}
--- tests/Posts/Year2017/Ast/ModifyTest.php
+++ tests/Posts/Year2017/Ast/ModifyTest.php
@@ -4,12 +4,20 @@ namespace TomasVotruba\Website\Tests\Posts\Year2017\Ast;
 
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\NodeFinder;
+use PhpParser\NodeTraverser;
 use PhpParser\Parser;
 use PhpParser\ParserFactory;
 use PHPUnit\Framework\TestCase;
+use TomasVotruba\Website\Posts\Year2017\Ast\NodeVisitor\ChangeMethodNameNodeVisitor;
 
 final class ModifyTest extends TestCase
 {
+    /**
+     * @var NodeTraverser
+     */
+    private $nodeTraverser;
+
     /**
      * @var Parser
      */
@@ -20,10 +28,18 @@ final class ModifyTest extends TestCase
      */
     private $srcDirectory;
 
+    /**
+     * @var NodeFinder
+     */
+    private $nodeFinder;
+
     protected function setUp(): void
     {
         $this->srcDirectory = __DIR__ . '/../../../../src/Posts/Year2017/Ast';
         $this->parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+
+        $this->nodeFinder = new NodeFinder;
+        $this->nodeTraverser = new NodeTraverser;
     }
 
     public function testParse(): void
@@ -37,5 +53,20 @@ final class ModifyTest extends TestCase
 
         $classMethodNode = $classNode->stmts[0];
         $this->assertInstanceOf(ClassMethod::class, $classMethodNode);
+    }
+
+    public function testNodeVisitor(): void
+    {
+        $this->nodeTraverser->addVisitor(new ChangeMethodNameNodeVisitor);
+
+        $nodes = $this->parser->parse(file_get_contents($this->srcDirectory . '/SomeClass.php'));
+        /** @var ClassMethod $classMethodNode */
+        $classMethodNode = $this->nodeFinder->findFirstInstanceOf($nodes, ClassMethod::class);
+        $this->assertSame('someMethod', $classMethodNode->name->toString());
+
+        $newNodes = $this->nodeTraverser->traverse($nodes);
+        /** @var ClassMethod $classMethodNode */
+        $classMethodNode = $this->nodeFinder->findFirstInstanceOf($newNodes, ClassMethod::class);
+        $this->assertSame('changedName', $classMethodNode->name->toString());
     }
 }
