014d8d6a96264c8d506da435cd724e10db21901e
TomasVotruba <tomas.vot@gmail.com>
2017-12-09 14:13:56 +0100

    [TweetPublisher] simplify with new Statie API

--- src/TweetPublisher/PostsProvider.php
+++ src/TweetPublisher/PostsProvider.php
@@ -2,77 +2,35 @@
 
 namespace TomasVotruba\Website\TweetPublisher;
 
-use Nette\Utils\Finder;
-use SplFileInfo;
-use Symplify\Statie\Renderable\Configuration\ConfigurationDecorator;
-use Symplify\Statie\Renderable\File\AbstractFile;
-use Symplify\Statie\Renderable\File\FileFactory;
+use Symplify\Statie\Application\StatieApplication;
+use Symplify\Statie\Configuration\Configuration;
 use Symplify\Statie\Renderable\File\PostFile;
-use Symplify\Statie\Renderable\Routing\RouteFileDecorator;
 
 final class PostsProvider
 {
     /**
-     * @var string
+     * @var StatieApplication
      */
-    private $postSource;
+    private $statieApplication;
 
     /**
-     * @var FileFactory
+     * @var Configuration
      */
-    private $fileFactory;
+    private $configuration;
 
-    /**
-     * @var ConfigurationDecorator
-     */
-    private $configurationDecorator;
-
-    /**
-     * @var RouteFileDecorator
-     */
-    private $routeFileDecorator;
-
-    public function __construct(
-        string $postSource,
-        FileFactory $fileFactory,
-        ConfigurationDecorator $configurationDecorator,
-        RouteFileDecorator $routeFileDecorator
-    ) {
-        $this->fileFactory = $fileFactory;
-        $this->configurationDecorator = $configurationDecorator;
-        $this->postSource = $postSource;
-        $this->routeFileDecorator = $routeFileDecorator;
+    public function __construct(StatieApplication $statieApplication, Configuration $configuration)
+    {
+        $this->statieApplication = $statieApplication;
+        $this->configuration = $configuration;
     }
 
     /**
-     * @return PostFile[]|AbstractFile[]
+     * @return PostFile[]
      */
     public function provide(): array
     {
-        return $this->getPostsWithConfigurationFromSource($this->postSource);
-    }
+        $this->statieApplication->run(__DIR__ . '/../../source', __DIR__ . '/../../output', true);
 
-    /**
-     * @return PostFile[]|AbstractFile[]
-     */
-    private function getPostsWithConfigurationFromSource(string $postSource): array
-    {
-        $files = $this->findMdFilesInDirectory($postSource);
-        $posts = $this->fileFactory->createFromFileInfos($files);
-        $this->configurationDecorator->decorateFiles($posts);
-        $this->routeFileDecorator->decorateFiles($posts);
-
-        return $posts;
-    }
-
-    /**
-     * @return SplFileInfo[]
-     */
-    private function findMdFilesInDirectory(string $postSource): array
-    {
-        /** @var Finder $finder */
-        $finder = Finder::findFiles('*.md')->from($postSource);
-
-        return iterator_to_array($finder->getIterator());
+        return $this->configuration->getOption('posts');
     }
 }
