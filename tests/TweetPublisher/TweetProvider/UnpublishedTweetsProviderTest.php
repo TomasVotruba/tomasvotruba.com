ab4db968d7115802fc7477115c59636c8b093f0d
TomasVotruba <tomas.vot@gmail.com>
2018-01-20 12:59:33 +0100

    tests

---
+++ tests/TweetPublisher/TweetProvider/UnpublishedTweetsProviderTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace TomasVotruba\Website\TweetPublisher\Tests\TweetProvider;
+
+use TomasVotruba\Website\TweetPublisher\Tests\AbstractContainerAwareTestCase;
+use TomasVotruba\Website\TweetPublisher\Tweet\Tweet;
+use TomasVotruba\Website\TweetPublisher\TweetProvider\UnpublishedTweetsProvider;
+
+final class UnpublishedTweetsProviderTest extends AbstractContainerAwareTestCase
+{
+    /**
+     * @var UnpublishedTweetsProvider
+     */
+    private $unpublishedTweetsProvider;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->unpublishedTweetsProvider = $this->container->get(UnpublishedTweetsProvider::class);
+    }
+
+    public function test(): void
+    {
+        $unpublishedPostTweets = $this->unpublishedTweetsProvider->provide();
+        $this->assertGreaterThanOrEqual(51, $unpublishedPostTweets);
+    }
+}
