c453a2878afbba427d851544f5546a75bce244b2
TomasVotruba <tomas.vot@gmail.com>
2018-01-20 13:13:43 +0100

    move TweetPublisher to TweetPublisherApplication, to state entry point

--- src/TweetPublisher/Application/TweetPublisherApplication.php
+++ src/TweetPublisher/Application/TweetPublisherApplication.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace TomasVotruba\Website\TweetPublisher;
+namespace TomasVotruba\Website\TweetPublisher\Application;
 
 use Symfony\Component\Console\Style\SymfonyStyle;
 use TomasVotruba\Website\TweetPublisher\Tweet\Tweet;
@@ -12,7 +12,7 @@ use TomasVotruba\Website\TweetPublisher\TwitterApi\TwitterApiWrapper;
  * @inspire https://gist.github.com/petrvacha/28ec8f5eac39283f1e7dce350f5a65ad
  * @thanks Petr Vacha
  */
-final class TweetPublisher
+final class TweetPublisherApplication
 {
     /**
      * @var int
