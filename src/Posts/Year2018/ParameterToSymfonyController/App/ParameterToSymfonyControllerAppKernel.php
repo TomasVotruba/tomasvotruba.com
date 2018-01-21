9995fcc71f8108b312617a6b59da069a33167868
TomasVotruba <tomas.vot@gmail.com>
2018-01-21 14:52:08 +0100

    init test

---
+++ src/Posts/Year2018/ParameterToSymfonyController/App/ParameterToSymfonyControllerAppKernel.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace TomasVotruba\Website\Posts\Year2018\ParameterToSymfonyController\App;
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+final class ParameterToSymfonyControllerAppKernel extends Kernel
+{
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): array
+    {
+        return [];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $loader->load(__DIR__ . '/../config/config.yml');
+    }
+
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir() . '/_tomas_votruba_blog_cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir() . '/_tomas_votruba_blog_log';
+    }
+}
---
+++ src/Posts/Year2018/ParameterToSymfonyController/Controller/LectureController.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace TomasVotruba\Website\Posts\Year2018\ParameterToSymfonyController\Controller;
+
+final class LectureController
+{
+    /**
+     * @var string
+     */
+    private $bankAccount;
+
+    public function __construct(string $bankAccount)
+    {
+        $this->bankAccount = $bankAccount;
+    }
+
+    /**
+     * Just for testing purposes,
+     * controller should not contain public methods except actions
+     */
+    public function getBankAccount(): string
+    {
+        return $this->bankAccount;
+    }
+}
