eed10340430769e37ffe9c5ac751b2f2709a00ec
Tomas Votruba <tomas.vot@gmail.com>
2018-06-21 19:04:33 +0200

    Twig2Latte: variables with array access

--- bin/latte-to-twig-syntax.php
+++ bin/latte-to-twig-syntax.php
@@ -23,7 +23,7 @@ foreach ($twigFileInfos as $twigFileInfo) {
     $content = $twigFileInfo->getContents();
 
     // 1. variables: {$google_analytics_tracking_id} => {{ $google_analytics_tracking_id }}
-    $content = Strings::replace($content, '#{\$([A-Za-z_]+)}#', '{{ $1 }}');
+    // $content = Strings::replace($content, '#{\$([A-Za-z_]+)}#', '{{ $1 }}');
 
     // 2. include: {include "_snippets/menu.latte"} => {% include "_snippets/menu.latte" %}
     // $content = Strings::replace($content, '#{include (["a-z_/.]+)}#', '{% include $1 %}');
@@ -34,7 +34,10 @@ foreach ($twigFileInfos as $twigFileInfo) {
     // 4. block: {block content}{/block} => {{ block content }}{/block}
     // $content = Strings::replace($content, '#{block ([A-Za-z_/"]+)}#', '{{ block $1 }}');
     // 5. /block: {/block} => {{ end block }}
-    $content = Strings::replace($content, '#{/block}#', '{{ endblock }}');
+    // $content = Strings::replace($content, '#{/block}#', '{{ endblock }}');
+
+    // 6. {$post['relativeUrl']} => {{ post.relativeUrl }}
+    $content = Strings::replace($content, '#{\$([A-Za-z_-]+)\[\'([A-Za-z_-]+)\'\]}#', '{{ $1.$2 }}');
 
     file_put_contents($twigFileInfo->getRealPath(), $content);
 }
