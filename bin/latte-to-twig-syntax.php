aab13390c7b5529853f5e3b20387590fc5e66754
Tomas Votruba <tomas.vot@gmail.com>
2018-06-21 18:41:13 +0200

    Twig2Latte: include

--- bin/latte-to-twig-syntax.php
+++ bin/latte-to-twig-syntax.php
@@ -19,8 +19,14 @@ $twigFileInfos = iterator_to_array($twigFilesFinder->getIterator());
 foreach ($twigFileInfos as $twigFileInfo) {
     dump($twigFileInfo->getRelativePathname());
     // rules
-    // 1. variables: {$google_analytics_tracking_id} => { $google_analytics_tracking_id }
-    $twigContent = Strings::replace($twigFileInfo->getContents(), '#{\$([a-z_]+)}#', '{{ $1 }}');
 
-    file_put_contents($twigFileInfo->getRealPath(), $twigContent);
+    $content = $twigFileInfo->getContents();
+
+    // 1. variables: {$google_analytics_tracking_id} => {{ $google_analytics_tracking_id }}
+    // $content = Strings::replace($content, '#{\$([a-z_]+)}#', '{{ $1 }}');
+
+    // 2. include: {include "_snippets/menu.latte"} => {% include "_snippets/menu.latte" %}
+    $content = Strings::replace($content, '#{include (["a-z_/.]+)}#', '{% include $1 %}');
+
+    file_put_contents($twigFileInfo->getRealPath(), $content);
 }
