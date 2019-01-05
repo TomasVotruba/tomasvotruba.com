23dbfa9803e2c360ccd81c0fdb4c03fa79dad478
Tomas Votruba <tomas.vot@gmail.com>
2019-01-05 22:27:04 +0100

    fix config paths

--- src/DependencyInjection/WebsiteKernel.php
+++ src/DependencyInjection/WebsiteKernel.php
@@ -18,7 +18,7 @@ final class WebsiteKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
-        $loader->load(__DIR__ . '/../config/config.yml');
+        $loader->load(__DIR__ . '/../../config/config.yml');
     }
 
     /**
