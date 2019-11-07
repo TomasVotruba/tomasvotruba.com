340c70f747afea602d5b123a28e3e0f073072a0e
Tomas Votruba <tomas.vot@gmail.com>
2019-11-07 18:53:51 +0100

    bump to Symfony 5

--- tests/Posts/Year2018/LatteTwig/LatteToTwigConverterTest.php
+++ tests/Posts/Year2018/LatteTwig/LatteToTwigConverterTest.php
@@ -6,8 +6,8 @@ namespace TomasVotruba\Website\Tests\Posts\Year2018\LatteTwig;
 
 use Symplify\LatteToTwigConverter\HttpKernel\LatteToTwigConverterKernel;
 use Symplify\LatteToTwigConverter\LatteToTwigConverter;
-use Symplify\PackageBuilder\FileSystem\SmartFileInfo;
 use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
 
 final class LatteToTwigConverterTest extends AbstractKernelTestCase
 {
@@ -18,7 +18,8 @@ final class LatteToTwigConverterTest extends AbstractKernelTestCase
         /** @var LatteToTwigConverter $latteToTwigConverter */
         $latteToTwigConverter = self::$container->get(LatteToTwigConverter::class);
 
-        $convertedContent = $latteToTwigConverter->convertFile(new SmartFileInfo(__DIR__ . '/Source/latte-file.latte'));
+        $fileInfo = new SmartFileInfo(__DIR__ . '/Source/latte-file.latte');
+        $convertedContent = $latteToTwigConverter->convertFile($fileInfo);
 
         $this->assertStringEqualsFile(__DIR__ . '/Source/expected-twig-file.twig', $convertedContent);
     }
