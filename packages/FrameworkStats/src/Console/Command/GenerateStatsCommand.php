760af5bfe94856ca1cb0a8e4b14137b87c5c6346
TomasVotruba <tomas.vot@gmail.com>
2020-01-03 01:34:07 +0100

    add mapper to array

--- packages/FrameworkStats/src/Console/Command/GenerateStatsCommand.php
+++ packages/FrameworkStats/src/Console/Command/GenerateStatsCommand.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symplify\PackageBuilder\Console\Command\CommandNaming;
 use Symplify\PackageBuilder\Console\ShellCode;
+use TomasVotruba\FrameworkStats\Mapper\VendorDataMapper;
 use TomasVotruba\FrameworkStats\Result\VendorDataFactory;
 use TomasVotruba\FrameworkStats\Yaml\YamlFileDumper;
 
@@ -36,18 +37,25 @@ final class GenerateStatsCommand extends Command
     private $vendorDataFactory;
 
     /**
+     * @var VendorDataMapper
+     */
+    private $vendorDataMapper;
+
+    /**
      * @param string[] $frameworksVendorToName
      */
     public function __construct(
         SymfonyStyle $symfonyStyle,
         YamlFileDumper $yamlFileDumper,
         VendorDataFactory $vendorDataFactory,
+        VendorDataMapper $vendorDataMapper,
         array $frameworksVendorToName
     ) {
         parent::__construct();
         $this->symfonyStyle = $symfonyStyle;
         $this->yamlFileDumper = $yamlFileDumper;
         $this->vendorDataFactory = $vendorDataFactory;
+        $this->vendorDataMapper = $vendorDataMapper;
         $this->frameworksVendorToName = $frameworksVendorToName;
     }
 
@@ -59,11 +67,15 @@ final class GenerateStatsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $vendorData = $this->vendorDataFactory->createVendorsData($this->frameworksVendorToName);
+        $vendorsData = $this->vendorDataFactory->createVendorsData($this->frameworksVendorToName);
+
+        foreach ($vendorsData['vendors'] as $key => $vendorData) {
+            $vendorsData['vendors'][$key] = $this->vendorDataMapper->mapObjectToArray($vendorData);
+        }
 
         $filePath = __DIR__ . '/../../../../../source/_data/generated/php_framework_trends.yaml';
 
-        $this->yamlFileDumper->dumpAsParametersToFile('php_framework_trends', $vendorData, $filePath);
+        $this->yamlFileDumper->dumpAsParametersToFile('php_framework_trends', $vendorsData, $filePath);
 
         $this->symfonyStyle->success('Data imported!');
 
---
+++ packages/FrameworkStats/src/Mapper/VendorDataMapper.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TomasVotruba\FrameworkStats\Mapper;
+
+use TomasVotruba\FrameworkStats\ValueObject\VendorData;
+
+final class VendorDataMapper
+{
+    /**
+     * @var PackageDataMapper
+     */
+    private $packageDataMapper;
+
+    public function __construct(PackageDataMapper $packageDataMapper)
+    {
+        $this->packageDataMapper = $packageDataMapper;
+    }
+
+    public function mapObjectToArray(VendorData $vendorData): array
+    {
+        $vendorDataArray['vendor_name'] = $vendorData->getVendorName();
+        $vendorDataArray['vendor_total_last_year'] = $vendorData->getVendorTotalLastYear();
+        $vendorDataArray['vendor_total_previous_year'] = $vendorData->getVendorTotalPreviousYear();
+        $vendorDataArray['last_year_trend'] = $vendorData->getLastYearTrend();
+
+        foreach ($vendorData->getPackagesData() as $packageData) {
+            $vendorDataArray['packages_data'][] = $this->packageDataMapper->mapObjectToArray($packageData);
+        }
+
+        return $vendorDataArray;
+    }
+}
