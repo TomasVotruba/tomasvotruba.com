614c5a238cdc31cefb2b29ab219c703cbda662ba
TomasVotruba <tomas.vot@gmail.com>
2020-03-14 13:31:17 +0100

    use PHP 7.4 in CI

--- src/Twig/GlobalVariablesTwigExtension.php
+++ src/Twig/GlobalVariablesTwigExtension.php
@@ -12,18 +12,6 @@ final class GlobalVariablesTwigExtension extends AbstractExtension
     public function __construct(Environment $environment, array $clusters, array $contributors = [])
     {
         $environment->addGlobal('contributors_count', count($contributors));
-
-        $environment->addGlobal('google_analytics_tracking_id', 'UA-46082345-1');
-
         $environment->addGlobal('clusters', $clusters);
-
-        $environment->addGlobal('site_title', 'Tomas Votruba');
-
-        $environment->addGlobal('disqus_shortname', 'itsworthsharing');
-
-        $environment->addGlobal(
-            'github_repository_tests_directory',
-            'https://github.com/TomasVotruba/tomasvotruba.com/tree/master/tests'
-        );
     }
 }
