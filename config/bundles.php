909281284f8a1f5a20618613b30a6af846983e30
TomasVotruba <tomas.vot@gmail.com>
2020-06-20 14:43:35 +0200

    remove flex tossed bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,4 @@
 return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symplify\ParameterNameGuard\ParameterNameGuardBundle::class => ['dev' => true, 'test' => true],
 ];
