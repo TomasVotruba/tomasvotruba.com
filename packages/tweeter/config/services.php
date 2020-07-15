e3252cd16fdb1f2e7b054a30c1d7d87520697452
TomasVotruba <tomas.vot@gmail.com>
2020-07-15 17:09:51 +0200

    move services YAML to PHP

---
+++ packages/tweeter/config/services.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Symplify\SmartFileSystem\FileSystemGuard;
+use TomasVotruba\Tweeter\TwitterApi\TwitterApiFactory;
+use TwitterAPIExchange;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set('twitter_name', '%env(TWEETER_NAME)%');
+
+    $parameters->set('twitter_minimal_gap_in_days', 1);
+
+    $parameters->set('twitter_maximal_days_in_past', 300);
+
+    $parameters->set('twitter_consumer_key', '%env(TWITTER_CONSUMER_KEY)%');
+
+    $parameters->set('twitter_consumer_secret', '%env(TWITTER_CONSUMER_SECRET)%');
+
+    $parameters->set('twitter_oauth_access_token', '%env(TWITTER_OAUTH_ACCESS_TOKEN)%');
+
+    $parameters->set('twitter_oauth_access_token_secret', '%env(TWITTER_OAUTH_ACCESS_TOKEN_SECRET)%');
+
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('TomasVotruba\Tweeter\\', __DIR__ . '/../src')
+        ->exclude([__DIR__ . '/../src/Exception/*', __DIR__ . '/../src/ValueObject/*']);
+
+    $services->set(TwitterAPIExchange::class, TwitterAPIExchange::class)
+        ->factory([ref(TwitterApiFactory::class), 'create']);
+
+    $services->set(FileSystemGuard::class);
+};
