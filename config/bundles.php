bcfad6349e784b85225928177dd127cc7fdd3440
TomasVotruba <tomas.vot@gmail.com>
2020-12-02 20:00:14 +0100

    cleanup

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symplify\SymfonyStaticDumper\SymfonyStaticDumperBundle::class => ['all' => true],
-    Symplify\PhpConfigPrinter\Bundle\PhpConfigPrinterBundle::class => ['all' => true],
-    Symplify\ComposerJsonManipulator\ComposerJsonManipulatorBundle::class => ['dev' => true, 'test' => true],
 ];
