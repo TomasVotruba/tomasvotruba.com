cf9a285be7cebedb0effa9659606160e922f8efb
TomasVotruba <tomas.vot@gmail.com>
2021-01-24 14:22:19 +0100

    make use of constants

--- config/packages/framework.php
+++ config/packages/framework.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symplify\Amnesia\Functions\env;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension('framework', [
--- config/packages/twig.php
+++ config/packages/twig.php
@@ -3,28 +3,30 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\Amnesia\ValueObject\Symfony\Extension\Twig\NumberFormat;
+use Symplify\Amnesia\ValueObject\Symfony\Extension\TwigExtension;
 use function Symplify\Amnesia\Functions\env;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('twig', [
-        'default_path' => '%kernel.project_dir%/templates',
-        'paths' => [
+    $containerConfigurator->extension(TwigExtension::NAME, [
+        TwigExtension::DEFAULT_PATH => '%kernel.project_dir%/templates',
+        TwigExtension::PATHS => [
             __DIR__ . '/../../packages/framework-stats/templates',
             __DIR__ . '/../../packages/cleaning-lady/templates',
             __DIR__ . '/../../packages/blog/templates',
             __DIR__ . '/../../packages/cleaning-lady/templates',
         ],
-        'globals' => [
+        TwigExtension::GLOBALS => [
             'google_analytics_tracking_id' => 'UA-46082345-1',
             'site_title' => 'Tomas Votruba',
             'site_url' => env('SITE_URL'),
             'disqus_shortname' => 'itsworthsharing',
         ],
         // see https://symfony.com/blog/new-in-symfony-2-7-default-date-and-number-format-configuration
-        'number_format' => [
-            'decimals' => 0,
-            'decimal_point' => '.',
-            'thousands_separator' => ' ',
+        TwigExtension::NUMBER_FORMAT => [
+            NumberFormat::DECIMALS => 0,
+            NumberFormat::DECIMAL_POINT => '.',
+            NumberFormat::THOUSANDS_SEPARATOR => ' ',
         ],
     ]);
 };
