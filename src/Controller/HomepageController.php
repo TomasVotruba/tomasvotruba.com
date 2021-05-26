da9cb4f39af88e6148053750f84d47dc2896f4e9
TomasVotruba <tomas.vot@gmail.com>
2021-05-26 18:15:36 +0100

    add quotes

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -11,11 +11,25 @@ use TomasVotruba\Website\ValueObject\RouteName;
 
 final class HomepageController extends AbstractController
 {
+    /**
+     * @var string
+     */
+    private const QUOTES = [
+        "If you don't want to do something, you'll find an excuse.<br>If you really do, you'll find a way.",
+        'Every day is a new life to a wise man.',
+    ];
+
     #[Route(path: '/', name: RouteName::HOMEPAGE)]
     public function __invoke(): Response
     {
+        $quotes = self::QUOTES;
+
+        $randomQuoteKey = array_rand($quotes);
+        $quote = $quotes[$randomQuoteKey];
+
         return $this->render('homepage.twig', [
             'title' => 'PHP Trainings, Rectoring and Posts',
+            'quote' => $quote,
         ]);
     }
 }
