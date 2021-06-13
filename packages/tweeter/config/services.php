ea250b35e979a38a773285b3e84d798487bc07be
Tomas Votruba <tomas.vot@gmail.com>
2021-06-13 14:07:58 +0200

    cleanup

--- packages/tweeter/config/services.php
+++ packages/tweeter/config/services.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 use function Symplify\Amnesia\Functions\env;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
 use Symplify\SmartFileSystem\FileSystemGuard;
@@ -16,6 +16,7 @@ return function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(OptionAlias::TWITTER_NAME, env('TWEETER_NAME'));
     $parameters->set(OptionAlias::TWITTER_MINIMAL_GAP_IN_HOURS, 12);
     $parameters->set(OptionAlias::TWITTER_MAXIMAL_DAYS_IN_PAST, 300);
+
     $parameters->set(OptionAlias::TWITTER_CONSUMER_KEY, env('TWITTER_CONSUMER_KEY'));
     $parameters->set(OptionAlias::TWITTER_CONSUMER_SECRET, env('TWITTER_CONSUMER_SECRET'));
     $parameters->set(OptionAlias::TWITTER_OAUTH_ACCESS_TOKEN, env('TWITTER_OAUTH_ACCESS_TOKEN'));
@@ -29,10 +30,10 @@ return function (ContainerConfigurator $containerConfigurator): void {
         ->autoconfigure();
 
     $services->load('TomasVotruba\Tweeter\\', __DIR__ . '/../src')
-        ->exclude([__DIR__ . '/../src/Exception/*', __DIR__ . '/../src/ValueObject/*']);
+        ->exclude([__DIR__ . '/../src/Exception', __DIR__ . '/../src/ValueObject']);
 
     $services->set(TwitterAPIExchange::class, TwitterAPIExchange::class)
-        ->factory([ref(TwitterApiFactory::class), 'create']);
+        ->factory([service(TwitterApiFactory::class), 'create']);
 
     $services->set(FileSystemGuard::class);
     $services->set(ParameterProvider::class);
