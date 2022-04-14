a31af63fd399d6b100722bd01684bf3ebbd57443
Tomas Votruba <tomas.vot@gmail.com>
2022-04-14 22:54:51 +0200

    Make use of ECSConfig 🥚 (#1301)
    * bump to Symplify 10.2
    
    * use new ECSConfig

--- ecs.php
+++ ecs.php
@@ -3,18 +3,12 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\Operator\UnaryOperatorSpacesFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-
-    // makes run 8-16x faster, depending on CPU cores :)
-    $parameters->set(Option::PARALLEL, true);
-
-    $parameters->set(Option::PATHS, [
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
         __DIR__ . '/config',
         __DIR__ . '/packages',
         __DIR__ . '/bin',
@@ -22,20 +16,16 @@ return function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/tests',
         __DIR__ . '/ecs.php',
     ]);
+    $ecsConfig->import(SetList::SYMPLIFY);
 
-    $parameters->set(Option::PARALLEL, true);
-
-    $parameters->set(Option::SKIP, [
+    $ecsConfig->skip([
         // symfony magic mess
         __DIR__ . '/config/bootstrap.php',
         __DIR__ . '/config/bundles.php',
         UnaryOperatorSpacesFixer::class,
     ]);
 
-    $containerConfigurator->import(SetList::PSR_12);
-    $containerConfigurator->import(SetList::COMMON);
-    $containerConfigurator->import(SetList::SYMPLIFY);
+    $ecsConfig->sets([SetList::PSR_12, SetList::COMMON, SetList::SYMPLIFY]);
 
-    $services = $containerConfigurator->services();
-    $services->set(LineLengthFixer::class);
+    $ecsConfig->rule(LineLengthFixer::class);
 };
