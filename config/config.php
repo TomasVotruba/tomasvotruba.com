75be5b9bef1fb8248792bfe1e4f606517a4bf8aa
Tomas Votruba <tomas.vot@gmail.com>
2022-05-30 22:17:46 +0200

    simplify configs

--- config/config.php
+++ config/config.php
@@ -10,7 +10,6 @@ use TomasVotruba\Website\ValueObject\Option;
 
 return function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../packages/*/config/*.php');
-    $containerConfigurator->import(__DIR__ . '/packages/*');
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::SITE_URL, '%env(SITE_URL)%');
@@ -28,4 +27,18 @@ return function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(ParameterProvider::class)
         ->arg('$container', service('service_container'));
+
+    $containerConfigurator->extension('framework', [
+        'secret' => '%env(APP_SECRET)%',
+    ]);
+
+    $containerConfigurator->extension('twig', [
+        'default_path' => '%kernel.project_dir%/templates',
+        'globals' => [
+            'google_analytics_tracking_id' => 'UA-46082345-1',
+            'site_title' => 'Tomas Votruba',
+            'site_url' => '%env(SITE_URL)',
+            'disqus_shortname' => 'itsworthsharing',
+        ],
+    ]);
 };
