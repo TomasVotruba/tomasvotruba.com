990f4e23143aef9e1030829bd332a808f319c61d
Tomas Votruba <tomas.vot@gmail.com>
2023-02-19 12:23:58 +0000

    kikc of rector config

--- rector-symfony-to-laravel.php
+++ rector-symfony-to-laravel.php
@@ -6,29 +6,19 @@ use Rector\Config\RectorConfig;
 use Rector\Doctrine\Rector\Class_\MoveCurrentDateTimeDefaultInEntityToConstructorRector;
 use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
 use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return function (RectorConfig $rectorConfig): void {
     $rectorConfig->importNames();
 
-    $rectorConfig->sets([
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::NAMING,
-        SetList::TYPE_DECLARATION,
-        LevelSetList::UP_TO_PHP_81,
-    ]);
-
     $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/packages']);
 
-    $rectorConfig->skip([
-        RemoveExtraParametersRector::class,
-        MoveCurrentDateTimeDefaultInEntityToConstructorRector::class,
-
-        // broken for DateTime interface
-        RenamePropertyToMatchTypeRector::class => [
-            __DIR__  . '/packages/blog/src/ValueObject/Post.php',
-        ],
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
+        'Symfony\Bundle\FrameworkBundle\Controller\AbstractController' => 'Illuminate\Routing\Controller',
     ]);
+
+    // in the making :)
+    $rectorConfig->ruleWithConfiguration(MethodCallToFuncCallRector::class);
 };
