1e4a5f2e45c0864a1b51796b286bd67aa1d8e424
Tomas Votruba <tomas.vot@gmail.com>
2023-02-23 11:02:07 +0000

    fix

--- app/Http/HttpKernel.php
+++ app/Http/HttpKernel.php
@@ -8,4 +8,15 @@ use Illuminate\Foundation\Http\Kernel;
 
 final class HttpKernel extends Kernel
 {
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array<string, array<int, class-string|string>>
+     */
+    protected $middlewareGroups = [
+        'web' => [
+        ],
+        'admin' => [
+        ],
+    ];
 }
