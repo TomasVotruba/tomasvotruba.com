a8c7cbdd2f28c0cbf79d03d59f70fa503d171844
Tomas Votruba <tomas.vot@gmail.com>
2023-02-23 09:54:56 +0000

    prepare image for posts

--- src/Controller/PostImageController.php
+++ src/Controller/PostImageController.php
@@ -4,53 +4,71 @@ declare(strict_types=1);
 
 namespace TomasVotruba\Website\Controller;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Imagine\Gd\Imagine;
 use Imagine\Image\Box;
+use Imagine\Image\FontInterface;
 use Imagine\Image\Palette\RGB;
 use Imagine\Image\Point;
 use Nette\Utils\Strings;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @see Inspiration https://og.beyondco.de/Fix%20your%20Laravel%20exceptions%20with%20AI.png?theme=light&md=1&body=Take%20advantage%20of%20OpenAI%20to%20enhance%20your%20Laravel%20error%20pages%20with%20AI-powered%20solutions.&fontSize=125px&isPost=1&author=Marcel%20Pociot&authorAvatar=https%3A%2F%2Fbeyondco.de%2Fimg%2Fmarcel.jpg&readDuration=11%20minute%20read
  */
 final class PostImageController extends Controller
 {
-    public function __invoke(Request $request): BinaryFileResponse
+    /**
+     * @var string
+     */
+    private const INTER_FONT = __DIR__ . '/../../public/assets/fonts/Inter-VariableFont_slnt,wght.ttf';
+
+    /**
+     * Downloaded from https://fonts.google.com/specimen/Source+Sans+Pro?query=Source+Sans+Pro
+     * @var string
+     */
+    private const SOURCE_FONT_FILE = __DIR__ . '/../../public/assets/fonts/SourceSansPro-Bold.ttf';
+
+    public function __construct(
+        private readonly Imagine $imagine
+    ) {
+    }
+
+    public function __invoke(string $title): BinaryFileResponse
     {
         // @see https://imagine.readthedocs.io/en/stable/
-        $title = $request->get('title');
-        $rgb = new RGB();
-        $imagine = new Imagine();
         $imageFilePath = __DIR__ . '/../../public/assets/images/posts/thumbnail/' . Strings::webalize($title) . '.png';
-        $box = new Box(2040, 1117);
-        $image = $imagine->create($box);
-        // downloaded from https://fonts.google.com/specimen/Source+Sans+Pro?query=Source+Sans+Pro
-        $blackColor = $rgb->color('000000');
-        $blackHeadlineFont = $imagine->font(
-            __DIR__ . '/../../public/assets/fonts/SourceSansPro-Bold.ttf',
-            100,
-            $blackColor
-        );
-        $drawer = $image->draw();
-        $drawer->text($title, $blackHeadlineFont, new Point(130, 340), 0, 1800);
 
-        $greenColor = $rgb->color('1a8917');
-        $greenTextFont = $imagine->font(
-            __DIR__ . '/../../public/assets/fonts/Inter-VariableFont_slnt,wght.ttf',
-            40,
-            $greenColor
-        );
-        //$drawer = $image->draw();
-        $drawer->text('Written by Tomas Votruba', $greenTextFont, new Point(130, 870), 0, 400);
+        $box = new Box(2040, 1117);
+        $image = $this->imagine->create($box);
+        $drawer = $image->draw();
+
+        $blackFont = $this->createFont(self::SOURCE_FONT_FILE, '000000', 100);
+        $drawer->text($title, $blackFont, new Point(130, 340), 0, 1800);
+
+        $greenFont = $this->createFont(self::INTER_FONT, '1a8917', 40);
+        $drawer->text('Written by Tomas Votruba', $greenFont, new Point(130, 870), 0, 400);
+
         // add my face :)
-        $faceImage = $imagine->open(__DIR__ . '/../../public/assets/images/tomas_votruba_circle.jpg');
+        $faceImage = $this->imagine->open(__DIR__ . '/../../public/assets/images/tomas_votruba_circle.jpg');
         $faceImage->resize(new Box(200, 200));
 
         $image->paste($faceImage, new Point(1700, 800));
         $image->save($imageFilePath);
-        return new BinaryFileResponse($imageFilePath);
+
+        return response()->file($imageFilePath);
+    }
+
+    private function createFont(string $fontFamilyFile, string $hexColor, int $fontSize): FontInterface
+    {
+        $rgb = new RGB();
+        $color = $rgb->color($hexColor);
+
+        return $this->imagine->font(
+            $fontFamilyFile,
+            $fontSize,
+            $color
+        );
     }
 }
