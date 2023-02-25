d02159ae7bc705e4db3fea434859875f2f10c2c2
Tomas Votruba <tomas.vot@gmail.com>
2023-02-25 20:57:32 +0000

    Make use of Punchcard :tada: (#1368)

--- config/app.php
+++ config/app.php
@@ -8,20 +8,19 @@ use Illuminate\Database\DatabaseServiceProvider;
 use Illuminate\Filesystem\FilesystemServiceProvider;
 use Illuminate\Foundation\Providers\ConsoleSupportServiceProvider;
 use Illuminate\Foundation\Providers\FoundationServiceProvider;
+use Illuminate\Queue\QueueServiceProvider;
 use Illuminate\Translation\TranslationServiceProvider;
 use Illuminate\View\ViewServiceProvider;
+use TomasVotruba\PunchCard\AppConfig;
 
-return [
-    'name' => env('APP_NAME', 'TomasVotruba'),
-    'env' => env('APP_ENV', 'production'),
-    'debug' => (bool) env('APP_DEBUG', false),
-    'url' => env('APP_URL', 'http://localhost'),
-    'asset_url' => env('ASSET_URL'),
-    'timezone' => 'UTC',
-    'locale' => 'en',
-    'fallback_locale' => 'en',
-
-    'providers' => [
+return AppConfig::make()
+    ->key(env('APP_KEY'))
+    ->name(env('APP_NAME', 'TomasVotruba'))
+    ->env(env('APP_ENV', 'production'))
+    ->debug((bool) env('APP_DEBUG', false))
+    ->url(env('APP_URL', 'http://localhost'))
+    ->timezone('UTC')
+    ->providers([
         // Laravel Framework Service Providers...
         CacheServiceProvider::class,
         ConsoleSupportServiceProvider::class,
@@ -31,6 +30,6 @@ return [
         ViewServiceProvider::class,
         TranslationServiceProvider::class,
         RouteServiceProvider::class,
-        \Illuminate\Queue\QueueServiceProvider::class,
-    ],
-];
+        QueueServiceProvider::class,
+    ])
+    ->toArray();
--- config/view.php
+++ config/view.php
@@ -2,30 +2,9 @@
 
 declare(strict_types=1);
 
-return [
-    /*
-    |--------------------------------------------------------------------------
-    | View Storage Paths
-    |--------------------------------------------------------------------------
-    |
-    | Most templating systems load templates from disk. Here you may specify
-    | an array of paths that should be checked for your views. Of course
-    | the usual Laravel view path has already been registered for you.
-    |
-    */
+use TomasVotruba\PunchCard\ViewConfig;
 
-    'paths' => [__DIR__ . '/../resources/views'],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Compiled View Path
-    |--------------------------------------------------------------------------
-    |
-    | This option determines where all the compiled Blade templates will be
-    | stored for your application. Typically, this is within the storage
-    | directory. However, as usual, you are free to change this value.
-    |
-    */
-
-    'compiled' => env('VIEW_COMPILED_PATH', realpath(storage_path('framework/views'))),
-];
+return ViewConfig::make()
+    ->paths([__DIR__ . '/../resources/views'])
+    ->compiled(env('VIEW_COMPILED_PATH', realpath(storage_path('framework/views'))))
+    ->toArray();
