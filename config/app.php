8934e47a964d3654fea73accf88730cf6deba849
Tomas Votruba <tomas.vot@gmail.com>
2023-03-01 13:02:33 +0000

    use directly disuqs

--- config/app.php
+++ config/app.php
@@ -1,15 +1,16 @@
 <?php
 
 declare(strict_types=1);
-use Illuminate\Session\SessionServiceProvider;
 
 use App\Providers\RouteServiceProvider;
+
 use Illuminate\Cache\CacheServiceProvider;
 use Illuminate\Database\DatabaseServiceProvider;
 use Illuminate\Filesystem\FilesystemServiceProvider;
 use Illuminate\Foundation\Providers\ConsoleSupportServiceProvider;
 use Illuminate\Foundation\Providers\FoundationServiceProvider;
 use Illuminate\Queue\QueueServiceProvider;
+use Illuminate\Session\SessionServiceProvider;
 use Illuminate\Translation\TranslationServiceProvider;
 use Illuminate\View\ViewServiceProvider;
 use TomasVotruba\PunchCard\AppConfig;
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -72,7 +72,16 @@
 
     <a name="comments"></a>
 
-    @include('_snippets.disqus_comments')
+    <div id="disqus_thread"></div>
+
+    <script>
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://itsworthsharing.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
 
     <script id="dsq-count-scr" src="https://itsworthsharing/disqus.com/count.js" async defer></script>
 @endsection
