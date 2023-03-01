7eff90628ca54b26a3e2d41642e516def567a4f5
Tomas Votruba <tomas.vot@gmail.com>
2023-03-01 12:18:32 +0000

    mics

--- resources/views/rss.blade.php
+++ resources/views/rss.blade.php
@@ -1,6 +1,13 @@
 @php
+<<<<<<< HEAD
     /** @var $posts \App\Entity\Post[] */
 @endphp
+=======
+    /** @var $posts \TomasVotruba\Website\Entity\Post[] */
+@endphp
+
+<?xml version="1.0" encoding="UTF-8" ?>
+>>>>>>> 2861e0a805 (mics)
 
 <?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0"
@@ -10,15 +17,26 @@
 >
     <channel>
         <title>Tomas Votruba writes about PHP and education</title>
+<<<<<<< HEAD
         <link>
         https://tomasvotruba.com/</link>
         <description>{{ \App\Enum\Design::BLOG_TITLE }}</description>
+=======
+        <link>https://tomasvotruba.com/</link>
+        <description>PHP, Communities  posts by Tomas Votruba</description>
+>>>>>>> 2861e0a805 (mics)
         <pubDate>{{ "now"|date('r') }}</pubDate>
         <atom:link href="https://tomasvotruba.com/rss.xml" rel="self" type="application/rss+xml"/>
 
         <lastBuildDate>{{ $most_recent_post_date_time_stamp }}</lastBuildDate>
 
+<<<<<<< HEAD
         @foreach ($posts as $post)
+=======
+        {{-- https://stackoverflow.com/a/29161205/1348344 --}}
+
+        @foreach($posts as $post)
+>>>>>>> 2861e0a805 (mics)
             @php
                 $post_absolute_url = route(\App\Enum\RouteName::POST_DETAIL, ['slug' => $post->getSlug()]);
             @endphp
