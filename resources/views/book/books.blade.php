3de25b85ae3415b42459d69891a320d1f41a8c2a
Tomas Votruba <tomas.vot@gmail.com>
2023-03-04 12:17:38 +0000

    route

--- resources/views/book/books.blade.php
+++ resources/views/book/books.blade.php
@@ -9,7 +9,7 @@
                 @php /** @var $book \App\Entity\Book */ @endphp
 
                 <div class="text-center col-12 col-md-6 mb-5 mt-4">
-                    <a href="{{ route('book-detail', ['slug' => $book->getSlug()]) }}">
+                    <a href="{{ route(\App\Enum\RouteName::BOOK_DETAIL, ['slug' => $book->getSlug()]) }}">
                         <img src="{{ $book->getCoverImage() }}" style="width: 17em; padding: .5em;margin:0 0 1em 0"
                              class="shadow" alt="Rector book cover">
                     </a>
@@ -17,13 +17,13 @@
                     <br>
 
                     <a
-                            href="{{ route('book-detail', ['slug' => $book->getSlug()]) }}"
-                            target="blank"
-                            @class([
-                                'btn btn-lg mt-4 mb-2',
-                                'btn-success' => $book->isFinished(),
-                                'btn-warning' => ! $book->isFinished(),
-                            ])
+                        href="{{ route(\App\Enum\RouteName::BOOK_DETAIL, ['slug' => $book->getSlug()]) }}"
+                        target="blank"
+                        @class([
+                            'btn btn-lg mt-4 mb-2',
+                            'btn-success' => $book->isFinished(),
+                            'btn-warning' => ! $book->isFinished(),
+                        ])
                     >
                         @if ($book->isFinished())
                             Buy a Copy
