bb4e2bfd48f15bd5d849e3b963f72861c2d5f7aa
Tomas Votruba <tomas.vot@gmail.com>
2023-03-07 16:39:39 +0100

    plan

---
+++ tests/Smoking/ControllerSmokeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Smoking;
+
+use App\Tests\AbstractTestCase;
+
+final class ControllerSmokeTest extends AbstractTestCase
+{
+    public function test(): void
+    {
+        // @todo how to automate this using the container?
+
+        // 1. get all route names
+        // 2. generate all GET urls
+        // 3. call the url
+        // 4. assert 200
+        // 5. todo tweet screenshot :)
+    }
+}
