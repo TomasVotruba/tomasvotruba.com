dd96b39f006764a1e7f1fc4695a64d2196afa80f
Tomas Votruba <tomas.vot@gmail.com>
2024-01-14 18:47:44 +0100

    tidy

--- ecs.php
+++ ecs.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([__DIR__ . '/config', __DIR__ . '/app', __DIR__ . '/tests', __DIR__ . '/ecs.php']);
-
-    $ecsConfig->sets([SetList::PSR_12, SetList::COMMON, SetList::SYMPLIFY, SetList::STRICT]);
-
-    $ecsConfig->rule(LineLengthFixer::class);
-};
+return ECSConfig::configure()
+    ->withPaths([__DIR__ . '/config', __DIR__ . '/app', __DIR__ . '/tests'])
+    ->withRootFiles()
+    ->withPreparedSets(psr12: true, common: true)
+    ->withRules([LineLengthFixer::class]);
