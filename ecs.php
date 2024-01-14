e275430b7d59e172303e90ea291168748a02c4eb
Tomas Votruba <tomas.vot@gmail.com>
2024-01-14 19:01:41 +0100

    tv bump (#1444)
    * bump ECS
    
    * enable risky again

--- ecs.php
+++ ecs.php
@@ -8,5 +8,5 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 return ECSConfig::configure()
     ->withPaths([__DIR__ . '/config', __DIR__ . '/app', __DIR__ . '/tests'])
     ->withRootFiles()
-    ->withPreparedSets(psr12: true, common: true)
+    ->withPreparedSets(psr12: true, common: true, strict: true)
     ->withRules([LineLengthFixer::class]);
