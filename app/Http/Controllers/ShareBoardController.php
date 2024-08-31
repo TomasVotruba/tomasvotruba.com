0e6d6933797605401ce7f2cd94dc9b9c5121d119
Tomas Votruba <tomas.vot@gmail.com>
2024-08-31 17:36:39 +0200

    prepare board

---
+++ app/Http/Controllers/ShareBoardController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Repository\PostRepository;
+use Illuminate\Contracts\View\View;
+use Illuminate\Routing\Controller;
+
+final class ShareBoardController extends Controller
+{
+    public function __construct(
+        private PostRepository $postRepository
+    ) {
+    }
+
+    public function __invoke(): View
+    {
+        return \view('share_board', [
+            'title' => 'Share board',
+            'posts' => $this->postRepository->fetchAll(),
+        ]);
+    }
+}
---
+++ resources/views/share_board.blade.php
@@ -0,0 +1,12 @@
+@extends('layout/layout_base')
+
+@section('content')
+    <div class="container">
+        <h1>Share board</h1>
+
+        @foreach ($posts as $post)
+            <textarea class="form-control mb-4 p-2" style="height: 8em">https://tomasvotruba.com/blog/{{ $post->getSlug() }}
+            </textarea>
+        @endforeach
+    </div>
+@endsection
