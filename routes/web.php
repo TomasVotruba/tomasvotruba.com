d3fa462a1ffdc0b5213e3c31bbf84d5a53a0e092
Tomas Votruba <tomas.vot@gmail.com>
2024-08-31 17:25:37 +0200

    cleanup few deprecated posts

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RssController;
