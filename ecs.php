43737afae0d0bd580381a9ef29ecdc0a4f55853f
Tomas Votruba <tomas.vot@gmail.com>
2024-09-08 17:44:28 +0200

    move /app to /src

--- ecs.php
+++ ecs.php
@@ -6,7 +6,7 @@ use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return ECSConfig::configure()
-    ->withPaths([__DIR__ . '/config', __DIR__ . '/app', __DIR__ . '/tests'])
+    ->withPaths([__DIR__ . '/config', __DIR__ . '/src', __DIR__ . '/tests'])
     ->withRootFiles()
     ->withPreparedSets(psr12: true, common: true, strict: true)
     ->withRules([LineLengthFixer::class]);
