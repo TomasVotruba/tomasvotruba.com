7df9fbeafca29a8186b084d583e710ecdb3c85d9
Tomas Votruba <tomas.vot@gmail.com>
2024-09-12 18:59:37 +0200

    tv tools (#1488)
    * add composer dependency analyser
    
    * fixup! add composer dependency analyser
    
    * add post

--- rector.php
+++ rector.php
@@ -7,6 +7,11 @@ use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return RectorConfig::configure()
     ->withPaths([__DIR__ . '/src', __DIR__ . '/bootstrap/app.php', __DIR__ . '/config', __DIR__ . '/tests'])
+    ->withSkip([
+        \Rector\CodeQuality\Rector\ClassMethod\InlineArrayReturnAssignRector::class => [
+            __DIR__ . '/src/Repository/ToolRepository.php',
+        ],
+    ])
     ->withImportNames(removeUnusedImports: true)
     ->withRules([DeclareStrictTypesRector::class])
     ->withPreparedSets(
