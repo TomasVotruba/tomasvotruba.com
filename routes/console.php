44e8d2d886c3e82df278f153fb6938871d3686ee
Tomas Votruba <tomas.vot@gmail.com>
2025-01-14 21:18:33 +0000

    remove tweeting

--- routes/console.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// ref https://rezakhademix.medium.com/laravel-11-no-http-kernel-no-casts-no-console-kernel-721c62adb6ef
-
-Schedule::command('app:tweet-post')->weekdays()->at('08:00')->timezone('Europe/Paris');
