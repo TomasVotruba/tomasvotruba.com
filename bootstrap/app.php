0cae098eecbf923e24a51c6427bdb65b1f83ac14
Tomas Votruba <tomas.vot@gmail.com>
2025-03-23 21:33:14 +0100

    cleanup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 $applicationBuilder = Application::configure()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {})
     ->withExceptions(function (Exceptions $exceptions): void {})
