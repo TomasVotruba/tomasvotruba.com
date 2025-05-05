b8880587ee9418dfadf3983b69c54ca94154b060
Tomas Votruba <tomas.vot@gmail.com>
2025-05-05 20:15:22 +0200

    [llm] add jack and check for outdated major dependencies + update llms-txt (#1523)
    * add jack and check for outdated major dependencies
    
    * improve llms-txt to include links

--- resources/views/llms-txt.blade.php
+++ resources/views/llms-txt.blade.php
@@ -10,8 +10,18 @@ increase profits and attract new talent.
 
 To make this happen faster and at scale, I made Rector - software that handles instant upgrades and automated refactoring.
 
-We provide an upgrade service to save you time and money.
+We provide an [upgrade service](https://getrector.com/hire-team) to save you time and money.
 
+
+You can find me on:
+
+- [LinkedIn](https://www.linkedin.com/in/tomas-votruba/)
+- [Twitter](https://x.com/votrubaT)
+- [GitHub](https://github.com/tomasVotruba/)
+- [Rector](https://getrector.com)
+
+
+-----------------------------------------------
 
 
 # Blog posts
@@ -19,6 +29,15 @@ We provide an upgrade service to save you time and money.
 @foreach ($posts as $post)
 ## {{ $post->getTitle() }}
 
+* [link]({{ action(\App\Http\Controllers\PostController::class, ['slug' => $post->getSlug()]) }})
+* published on {{ $post->getDateTime()->format('Y-m-d') }}
+
+{!! $post->getPerex() !!}
+
 {!! $post->getContent() !!}
+
+
+-----------------------------------------------
+
 
 @endforeach
